93cf63da9d7a3f6b45ef5313ad66c94b8a81c06b
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-20 16:42:13 -0700

    Remove testing ajax calls and add session api ajax utils

--- frontend/dailyplanner.jsx
+++ frontend/dailyplanner.jsx
@@ -3,7 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as SessionAPI from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
+  window.signup = SessionAPI.signup;
+  window.login = SessionAPI.login;
+  window.logout = SessionAPI.logout;
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>From React</h1>, root);
 });
