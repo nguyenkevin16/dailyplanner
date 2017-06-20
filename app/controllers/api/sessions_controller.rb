fdcc49cb9c5950bbc1270e78681602ce05f06169
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-19 22:42:11 -0700

    Add sessions controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(session_params[:username], session_params[:password])
+
+    if @user
+      login(@user)
+      render :show
+    else
+      render json: ['Invalid credentials'], status: 401
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout
+      render json: {}, status: 200
+    else
+      render json: ['Not logged in.'], status: 404
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:username, :password)
+  end
+end
