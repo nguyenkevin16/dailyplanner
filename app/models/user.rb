6c1219fbb00bde108d502d74eb71e9fd8c856870
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-19 22:32:12 -0700

    Add users table/model and update gemfile

---
+++ app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ActiveRecord::Base
+  after_initialize :ensure_session_token, :ensure_avatar_img
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  has_many :tracks, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def ensure_avatar_img
+    self.avatar_url ||= 'https://res.cloudinary.com/nguyenkevin16/image/upload/v1484264933/System/default_avatar_zbymyn.jpg'
+  end
+
+  attr_reader :password
+end
