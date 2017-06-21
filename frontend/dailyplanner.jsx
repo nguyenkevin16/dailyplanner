706bde267c1eaa1c69c49f282126a3c17a6509f7
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-20 17:34:56 -0700

    Add application store

--- frontend/dailyplanner.jsx
+++ frontend/dailyplanner.jsx
@@ -2,13 +2,19 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 import * as SessionAPI from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // for testing
+  const store = configureStore();
   window.signup = SessionAPI.signup;
   window.login = SessionAPI.login;
   window.logout = SessionAPI.logout;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // 
 
   const root = document.getElementById('root');
   ReactDOM.render(<h1>From React</h1>, root);
