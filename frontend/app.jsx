85d024fd65c801911e2915f4387e7d075bf3880b
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-21 20:51:26 -0700

    Add background gradient

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 import Splash from './components/splash';
 
 const App = () => (
-  <Splash/>
+  <section className='app background-gradient'>
+    <Splash/>
+  </section>
 );
 
 export default App;
