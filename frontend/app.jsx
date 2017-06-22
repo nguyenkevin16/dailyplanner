b3cb50d09569cfd19c3659f5f20312401099a4eb
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-21 22:34:19 -0700

    Add basic featured technologies section

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,11 +1,14 @@
 // frontend/components/app.jsx
 
 import React from 'react';
+
 import Splash from './components/splash';
+import Tech from './components/tech';
 
 const App = () => (
   <section className='app'>
     <Splash/>
+    <Tech/>
   </section>
 );
 
