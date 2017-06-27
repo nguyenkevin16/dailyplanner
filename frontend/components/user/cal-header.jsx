25769fcc716fae53773010a9010b6ccc88bae5a8
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-27 16:34:13 -0700

    Fix vertical alignment for calendar

--- frontend/components/user/cal-header.jsx
+++ frontend/components/user/cal-header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CalHeader = (props) => {
 
   return(
-    <div className='cal-row flex-grid'>       
+    <div className='cal-header cal-row flex-grid'>       
       <span className='cal-cell flex-grid'> { '<' } </span>
       <span className='cal-title flex-grid'> { props.monthNames[props.month] } { props.year } </span>
       <span className='cal-cell flex-grid'> { '>' } </span>
--- frontend/components/user/cal-weekdays.jsx
+++ frontend/components/user/cal-weekdays.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CalWeekdays = (props) => {
 
   return(
-    <div className='cal-row flex-grid'>
+    <div className='cal-weekdays cal-row flex-grid'>
       {
         props.dayNames.map(day => <div className='cal-cell flex-grid' key={ day }> { day } </div> )
       }
