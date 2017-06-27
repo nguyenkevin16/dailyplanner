ee8246aa3e4c6a94b4ecddd24098510358e6f1d5
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-27 16:28:01 -0700

    Fix rendering of calendar dates

--- frontend/components/user/cal-dates.jsx
+++ frontend/components/user/cal-dates.jsx
@@ -1,16 +1,56 @@
 import React from 'react';
 
-const CalDates = (props) => {
+class CalDates extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return(
-    <section className='cal-dates flex-grid'>       
-      <div className='cal-row'>
-        
-      </div>
+    this.createDates = this.createDates.bind(this);
+  }
 
-      <div className='cal-row'/>
-    </section>
-  );
+  createDates(startDay, daysInMonth) {
+    let dates = [];
+    let splicedDates = [];
+
+    for (let i = 0; i < startDay; i++) {
+      dates.push(
+        <div className='cal-cell cal-date flex-grid' key={ 'blank-' + i }></div>
+      );
+    }
+
+    for (let j = 1; j <= daysInMonth; j++ ) {
+      dates.push(
+        <div className='cal-cell cal-date flex-grid' key={ 'date-' + j }>
+          { j }
+        </div>
+      );
+    }
+    
+    while (dates.length > 7) {
+      splicedDates.push(dates.splice(0, 7));
+    }
+
+    for (let k = dates.length; k < 7; k++) {
+      dates.push(
+        <div className='cal-cell cal-date flex-grid' key={ 'blank-' + k }></div>
+      )
+    }
+
+    splicedDates.push(dates);
+
+    return splicedDates.map((dates, idx) => (
+      <div className='cal-row flex-grid' key={ 'row-' + idx }> { dates } </div> 
+    ));
+  }
+
+  render() {
+    return(
+      <section className='cal-dates flex-grid'>       
+        {
+          this.createDates(this.props.startDay, this.props.daysInMonth)
+        }
+      </section>
+    );
+  }
 }
 
 export default CalDates;
